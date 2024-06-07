0e1a6545fa2d7b69e97e1bf51aae10a7f9d1a3f7
Brian Arnold <barnold@wellframe.com>
2024-06-07 16:21:00 -0400

    Make a temporary change that throws an error if the core data object's gs_data wound up being nil when loaded from storage.

---
+++ Tests/GarageStorageTests/RawStorageTests.swift
@@ -0,0 +1,38 @@
+//
+//  RawStorageTests.swift
+//  
+//
+//  Created by Brian Arnold on 6/7/24.
+//
+
+import XCTest
+@testable import GarageStorage
+
+class TestCoreData: Codable { }
+
+final class RawStorageTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        let garage = Garage()
+        garage.deleteAllObjects()
+    }
+
+    /// Test what happens if a core data objeect is created, but its `gs_data` never got set.
+    func testRawStorage() throws {
+        let garage = Garage()
+              
+        do {
+            _ = garage.makeCoreDataObject("TestCoreData", identifier: "TestIdentifier")
+            garage.save()
+        }
+        
+        do {
+            let _: TestCoreData? = try garage.retrieve(TestCoreData.self, identifier: "TestIdentifier")
+        } catch {
+            // we want to catch the error, and verify it
+            XCTAssertTrue(error.localizedDescription.contains("failed to retrieve gs_data"))
+        }
+        
+    }
+
+}
