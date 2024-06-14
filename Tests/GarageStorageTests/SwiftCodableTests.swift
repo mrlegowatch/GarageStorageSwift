f7e51202dc9aed6fe17a338b8f97ac99747858e9
Brian Arnold <barnold@wellframe.com>
2024-06-14 14:42:19 -0400

    Updated the tests to handle all of the scenarios (Swift + Codable, Swift + MappableObject, Swift emulating ObjC + MappableObject).

--- Tests/GarageStorageTests/SwiftCodableTests.swift
+++ Tests/GarageStorageTests/SwiftCodableTests.swift
@@ -373,8 +373,8 @@ class SwiftCodableTests: XCTestCase {
         let garage = Garage()
         
         do {
-            let sam = try garage.retrieve(SwiftPerson.self, identifier: "Sam")
-            XCTAssertNil(sam, "Should be nil")
+            let frodo = try garage.retrieve(SwiftPerson.self, identifier: "Frodo")
+            XCTAssertNil(frodo, "Should be nil")
         }
         catch {
             XCTFail("Should not have thrown an error: \(error)")
