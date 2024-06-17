b3020157a2b3497470583df8b716df442e5c0062
Brian Arnold <barnold@wellframe.com>
2024-06-17 13:05:26 -0400

    Bump up the swift-tools-version, and platform iOS and macOS minimum versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "GarageStorage",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_11)
+        .iOS(.v15),
+        .macOS(.v10_13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
