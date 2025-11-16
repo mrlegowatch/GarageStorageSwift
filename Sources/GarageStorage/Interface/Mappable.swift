c6b6a95aca53cfd661a50ec415e3246f799f1ed3
Brian Arnold <mrlegowatch@mac.com>
2025-11-16 08:17:23 -0500

    Updated comments and documentation to match the recent changes.

--- Sources/GarageStorage/Interface/Mappable.swift
+++ Sources/GarageStorage/Interface/Mappable.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 /// An optional, convenience protocol that conforms to `Codable` and `Identifiable` where `ID` conforms to `LosslessStringConvertible`.
-/// These two protocol conformances are required for uniquely identified objects in a Garage.
-/// Most clients can simply conform to `Codable` and `Identifiable` directly, to meet these requirements.
-/// This protocol is only required for backward compatibility with existing code already using Mappable.
+/// These two protocol conformances are required for parking and retrieving uniquely identified objects in a Garage.
+///
+/// - note: Most clients can simply conform to `Codable` and `Identifiable` directly, to meet these requirements.
+/// This protocol is only required for backward compatibility with existing code already using `Mappable`.
 public protocol Mappable: Codable, Identifiable where ID: LosslessStringConvertible { }
